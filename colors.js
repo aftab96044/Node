2111b1cb792d87503983ba2bd53686adf2d4d644
aftab96044 <aftab.neymar.10@gmail.com>
2026-06-10 11:06:29 +0530

    colors

---
+++ colors.js
@@ -0,0 +1,5 @@
+const colors = require('colors');
+
+console.log(colors.red("ajsdh asidoai"));
+console.log(colors.blue("asdjoaso sfdjsad"));
+console.log(colors.green("asuidhas asiduaos... asjjdh"));

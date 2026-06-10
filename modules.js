7d12b1ef73ec1fddbcb8dc8ff6d642e3956afd39
aftab96044 <aftab.neymar.10@gmail.com>
2026-06-10 11:06:42 +0530

    module

---
+++ modules.js
@@ -0,0 +1,26 @@
+// const fs = require('fs');
+
+// fs.writeFileSync("dummy.txt","trying and learning modules")
+
+
+// const os = require('os')
+
+// console.log(os.platform());
+// console.log(os.hostname);
+// console.log(os.cpus);
+
+
+
+// Global objects are those which need not to be installed. ex:-console
+// core modules are exactly the same
+
+
+
+const {log} = require('console')
+
+log("boom boom ciao")
+
+
+
+
+
